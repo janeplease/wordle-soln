e22d3af9e8f6113204595a3e230004426eaa815f
janeplease <badassessment1@gmail.com>
2023-01-10 10:10:58 +0530

    basic of rest api

--- be/be/controller.py
+++ be/be/controller.py
@@ -1,5 +1,27 @@
 from be import app
+from flask import request
 
-@app.route("/e")
-def home1():
+@app.route("/postcodedword/<name>", methods = ['POST'])
+def get_coded_word():
+    name = request.args.get('name')
+    print(name)
+
+
+@app.route("/getnewword", methods = ['GET'])
+def send_new_word():
     return "Hello, Flask!!!!!!"
+
+
+@app.route("/getnewword1", methods = ['GET'])
+def send_new_word1():
+    return "Hello, Flask!!!!!!"
+
+
+@app.route("/post1", methods = ['POST'])
+def get_coded_word1():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        return json
+    else:
+        return 'Content-Type not supported!'
